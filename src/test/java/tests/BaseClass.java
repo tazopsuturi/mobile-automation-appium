6959ebfb759d728ee51759cdb1b5a6783edc4ddf
TazoPsuturi <tazopsuturi33@gmail.com>
2025-02-18 13:10:06 +0400

    fixed appium server starter code

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -8,21 +8,21 @@ public class BaseClass {
 	public static void main(String[] args) {
 		AndroidDriver driver = null;
 		try {
-			UiAutomator2Options options = new UiAutomator2Options()
-					.setPlatformName("Android")
-					.setAutomationName("UiAutomator2")
-					.setUdid("emulator-5554")
-					.setAppPackage("com.wdiodemoapp")
-					.setAppActivity("com.wdiodemoapp.MainActivity");
+			UiAutomator2Options caps = new UiAutomator2Options();
+			caps.setCapability("platformName", "Android");
+			caps.setCapability("appium:automationName", "UiAutomator2");
+			caps.setCapability("appium:deviceName", "emulator-5554");  // Ensure the device is available
+			caps.setCapability("appium:appPackage", "com.wdiodemoapp");
+			caps.setCapability("appium:appActivity", "com.wdiodemoapp.MainActivity");
+
 			
-			URL appiumServer = new URL("http://127.0.0.1:4723");
-			driver = new AndroidDriver(appiumServer, options);
+			URL appiumServer = new URL("http://127.0.0.1:4723/wd/hub");
+			
+			driver = new AndroidDriver(appiumServer, caps);
 			System.out.println("Session started!");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (driver != null) driver.quit();
 		}
 	}
 }
